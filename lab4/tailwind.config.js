76c7150b5b77c4812a6f8c0ea93001a62811ba88
GamerRiT <alex-kashin-03@mail.ru>
2021-03-30 23:04:20 +0300

    -

---
+++ lab4/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens:{
+      'lol':'1296px',
+    },
+    colors:{
+      'green01':'4D533C',
+    },
+    extend: {
+      screen: {
+        '3xl':'1440'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
